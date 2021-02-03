c445eb12780cbcd52a00de05b5e9467fc65b6235
EsioFreitas <esio.gustavo@gmail.com>
2021-02-03 09:17:28 -0300

    feat: add exemple component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import GlobalStyles from 'assets/styles/global';
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
