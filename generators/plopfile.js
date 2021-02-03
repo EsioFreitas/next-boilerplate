0f1dd1d649ac951889ce6392eef375bee6398fa3
EsioFreitas <esio.gustavo@gmail.com>
2021-02-03 10:56:03 -0300

    feat: add stories template

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,32 +1,32 @@
 module.exports = function (plop) {
-  plop.setGenerator('component', {
-    description: 'application component logic',
+  plop.setGenerator('components', {
+    description: 'application components logic',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'component name please'
+        message: 'components name please'
       }
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/component/{{pascalCase name}}/index.tsx',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/index.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/component/{{pascalCase name}}/stories.tsx',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
         templateFile: 'templates/index.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/component/{{pascalCase name}}/styles.ts',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/index.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/component/{{pascalCase name}}/test.tsx',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
         templateFile: 'templates/index.tsx.hbs'
       }
     ]
